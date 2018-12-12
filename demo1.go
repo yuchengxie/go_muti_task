deca5e192c4159f1636dfde8cb7964e3c02790e8
yuchengxie <648547525@qq.com>
2018-12-13 00:37:41 +0800

    add demo1 demo2

--- demo1.go
+++ demo1.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/exec"
+)
 
+func main() {
+	var (
+		cmd *exec.Cmd
+	)
+	cmd=exec.Command("/bin/bash","-c","echo1;echo2")
+	err:=cmd.Run()
+	fmt.Println(err)
+	//if err!=nil{
+	//	fmt.Println(err)
+	//}
 }
+
+
+
+
+
+
+
+
+
+
+
+
